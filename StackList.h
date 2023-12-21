d952a7de3adc19e9a6a872d7b82d170e8071a7aa
Mohamed Mostafa <138163382+MohamedMostafa259@users.noreply.github.com>
2023-12-22 01:20:36 +0200

    Add files via upload
    Added the StackList.h implementation for task 2

---
+++ StackList.h
@@ -0,0 +1,45 @@
+#ifndef STACK_LIST_H
+#define STACK_LIST_H
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+template <typename T>
+class StackList {
+private:
+    list<T> l;
+
+public:
+    
+    void push(const T& val) {
+        l.push_back(val);
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            T res = l.back();
+            l.pop_back();
+            return res;
+        }
+        cout << "Stack is Empty!!\n";
+        return T(); // return default value
+    }
+
+    T top() {
+        if (!isEmpty()) 
+            return l.back();
+        cout << "Stack is Empty!!\n";
+        return T(); // return default value
+    }
+
+    bool isEmpty() {
+        return l.empty();
+    }
+
+    void clear() {
+        l.clear();
+    }
+};
+
+#endif /* STACK_LIST_H */
