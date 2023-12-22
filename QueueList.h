8d37de5259127c24550faffe789f593211931f8e
MohamedKarim28 <121764945+MohamedKarim28@users.noreply.github.com>
2023-12-22 14:14:50 +0200

    Create QueueList.h

---
+++ QueueList.h
@@ -0,0 +1,41 @@
+#ifndef DATA_STRUCTURE_QUEUE_H
+#define DATA_STRUCTURE_QUEUE_H
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+template <typename T>
+class TemplateQueueList {
+        private:
+        list<T> elements;
+
+        public:
+        void enqueue(const T& value) {
+            elements.push_back(value);
+        }
+
+        void dequeue() {
+            if (!isEmpty()) {
+                elements.pop_front();
+            }
+        }
+
+        T front() {
+            if (!isEmpty()) {
+                return elements.front();
+            } else {
+                return T();
+            }
+        }
+
+        bool isEmpty() {
+            return elements.empty();
+        }
+
+        void clear() {
+            elements.clear();
+        }
+};
+
+#endif //DATA_STRUCTURE_QUEUE_H
