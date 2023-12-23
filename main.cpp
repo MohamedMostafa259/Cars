9046a44c006df3d7866e26bccffed8904e0babdf
22-101058 <154559703+22-101058@users.noreply.github.com>
2023-12-23 20:49:19 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "QueueProject.h"
+#include "StackList.h"
+#include "task1_backend.h"
+#include "task2_backend.h"
+using namespace std;
+int main() {
+    // Test for Function: (2)
+
+
+    // Test for Function: (3)
+
+
+    // Test for Function: (4)
+    cout << convertToNS(61, 3) << "\n"; // 2021
+    cout << convertToNS(0, 3) << "\n"; // 0
+    cout << convertToNS(564231, 16) << "\n"; // 89C07
+    cout << convertToNS(564231, 10) << "\n"; // 564231
+    cout << convertToNS(564231, 8) << "\n"; // 2116007
+    cout << convertToNS(564231, 2) << "\n"; // 10001001110000000111
+    cout << convertToNS(125, 18) << "\n"; // 6H
+    cout << convertToNS(125, 1) << "\n"; // Error!! The Allowed Range for Base is [2, 20]
+
+    // Test for Function: (5)
+    cout << "===========" << endl;
+    subtractLargeInts("19547465462", "5555555");
+
+    // Test for Function: (6)
+
+
+    return 0;
+}
