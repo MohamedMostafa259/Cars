d315a2f95adf7598b68172f61ab7c9567b6e22bc
22-101058 <154559703+22-101058@users.noreply.github.com>
2023-12-23 20:50:12 +0200

    Update task2_backend.h

--- task2_backend.h
+++ task2_backend.h
@@ -107,31 +107,6 @@ string subtractLargeInts (const string &num1, const string &num2) {
 // (6)
 
 
-int main() {
-    // Test for Function: (2)
 
-
-    // Test for Function: (3)
-
-
-    // Test for Function: (4)
-    cout << convertToNS(61, 3) << "\n"; // 2021
-    cout << convertToNS(0, 3) << "\n"; // 0
-    cout << convertToNS(564231, 16) << "\n"; // 89C07
-    cout << convertToNS(564231, 10) << "\n"; // 564231
-    cout << convertToNS(564231, 8) << "\n"; // 2116007
-    cout << convertToNS(564231, 2) << "\n"; // 10001001110000000111
-    cout << convertToNS(125, 18) << "\n"; // 6H
-    cout << convertToNS(125, 1) << "\n"; // Error!! The Allowed Range for Base is [2, 20]
-
-    // Test for Function: (5)
-    cout << "===========" << endl;
-    subtractLargeInts("19547465464", "5555555");
-
-    // Test for Function: (6)
-
-
-    return 0;
-}
 
 #endif /* TASK2_BACKEND_H */
